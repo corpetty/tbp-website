74c4d3de27b821c6d29b4df92d9beaba8e9fc6e8
Corey Petty <corey@status.im>
2025-01-06 11:44:18 -0500

    added TBP Logo and nav page

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -16,14 +16,14 @@ export const Logo = (props: Props) => {
   return (
     /* eslint-disable @next/next/no-img-element */
     <img
-      alt="Payload Logo"
-      width={193}
-      height={34}
+      alt="The Bitcoin Podcast Logo"
+      width={320}
+      height={53}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      className={clsx('w-[320px] h-[53px]', className)}
+      src="/media/june2017logo.png"
     />
   )
 }
